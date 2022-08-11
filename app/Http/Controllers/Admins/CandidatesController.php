68e25036eaa31ad0ac62ad148af9b32f1f2641b4
H-navi <hanafinaufal@protonmail.com>
2022-08-11 13:34:27 +0700

    store data candidate

--- app/Http/Controllers/Admins/CandidatesController.php
+++ app/Http/Controllers/Admins/CandidatesController.php
@@ -7,6 +7,7 @@ use App\Models\Candidate;
 use App\Models\Job;
 use App\Models\Jobs;
 use App\Models\Skill;
+use App\Models\Skill_set;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -24,11 +25,11 @@ class CandidatesController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
+    {        
         return Inertia::render('Admins/Candidates/Index', [
-            'jobs' => Job::whereNotNull('deleted_at')->whereNotNull('deleted_by')->all(),
-            'skills' => Skill::whereNotNull('deleted_at')->whereNotNull('deleted_by')->all(),
-            'candidates' => Candidate::whereNotNull('deleted_at')->whereNotNull('deleted_by')->all(),
+            'jobs' => Job::whereNull('deleted_at')->orWhereNull('deleted_by')->get(),
+            'skills' => Skill::whereNull('deleted_at')->orWhereNull('deleted_by')->get(),
+            'candidates' => Candidate::whereNull('deleted_at')->orWhereNull('deleted_by')->get(),
         ]);
     }
 
@@ -50,22 +51,38 @@ class CandidatesController extends Controller
      */
     public function store(Request $request)
     {
+
+        // $skills = $request->skills[0];
+        // foreach($skills as $skill):
+        //     var_dump($skill['id']);
+        // endforeach;
+        // die;
         if (auth()->user()->hasAnyRole(['super-admin', 'admin'])) {
             $this->validate($request, [
                 'job' => 'required',
                 'name' => ['required', 'max:100'],
                 'email' => ['required', 'string', 'email', 'max:50', 'unique:users'],
                 'phone' => ['required', 'max:15'],
-                'year' => ['required', 'max:10'],
+                'year' => ['required', 'max:4'],
+                'skills' => 'required',
             ]);
-            $moodel = Candidate::create([
-                'job_id' => $request->job,
+            $model = Candidate::create([
+                'job_id' => $request->job[0]['id'],
                 'name' => $request->name,
                 'email' => $request->email,
                 'phone' => $request->phone,
                 'year' => $request->year,
                 'created_by' => Auth::id(),
             ]);
+
+            $skills = $request->skills[0];
+            foreach($skills as $skill):
+                $store_skill = Skill_set::create([
+                    'candidate_id' => $model->id,
+                    'skill_id' => $skill['id'],
+                ]);
+            endforeach;
+
             return back();
         }
         return back();
@@ -138,7 +155,7 @@ class CandidatesController extends Controller
         // }
         $candidate->update([
             'deleted_by' => Auth::id(),
-            'deleted_at' => Carbon::now()->format('d-m-Y'),
+            'deleted_at' => Carbon::now(),
         ]);
         return back();
     }
